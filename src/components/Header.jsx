ee547fca54364bb5465d69ae61bc6ec6de5e35ce
francisco <franciscouberto@gmail.com>
2023-04-30 11:25:14 +0200

    ico added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,17 @@
 
 import { Link } from "react-router-dom"
+import FL_logo from "../img/FL_logo.png"
+import "../Styles/Home.css"
 import "../Styles/App.css"
 
 export default function Header() {
     return (
         <Link to={'/'} className="header_link">
             <header className="header_container">
-                <h1>flock-lab</h1>
+                <div className="title_container">
+                    <img src={FL_logo} alt="FlockLab logo" />
+                    <h1>flock-lab</h1>
+                </div>
                 <p>Designing your digital presence</p>
             </header>
         </Link>
